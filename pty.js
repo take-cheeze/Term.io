d18165389063ade410f510b7989143763a072b9a
Craig Roddin <craig.roddin@gmail.com>
2011-03-08 16:41:03 -0700

    now can use ssl

--- pty.js
+++ pty.js
@@ -1,33 +1,35 @@
 
-"use strict"
+// "use strict"
 var spawn = require('child_process').spawn;
 var fs = require('fs');
 var connect = require('connect');
 var io = require('socket.io');
 
-var server = connect.createServer(
-    connect.static(__dirname + '/static'),
-	function(request, response) {
-		fs.readFile('./terminal.html', function(err, buffer) {
-			response.writeHead(200, {
-				'Content-Length': buffer.length,
-				'Content-Type': 'text/html; charset=utf-8'
-			});
-			response.end(buffer);
-		})
-	}
-);
+var port = parseInt(process.argv[2] || '8080', 10);
+var sslKeyPath = 'privatekey.pem';
+var sslCertPath = 'certificate.pem';
+var useSSL = true;
 
-var io = io.listen(server)
- 
+var server;
+if(useSSL){
+	server = connect({
+		key: fs.readFileSync(sslKeyPath),
+		cert: fs.readFileSync(sslCertPath)
+	});
+} else {
+	server = connect();
+}
+
+server.use(connect['static'](__dirname + '/static'));
+var io = io.listen(server);
+
 var ptys = {};
-
 io.on('connection', function(client){
-	client.initialized = false
+	client.initialized = false;
 	client.on('message', function(data){
 		//The first message sent by the client must be the term they want
 		if(!client.initialized){
-			var path = data
+			var path = data;
 			var term = spawn('python', ['-c', 'import pty;pty.spawn(["bash","-l"])']);
 			// or spawn login or just "bash"
 			//It might be better to link to the connections rather than storing a count
@@ -39,7 +41,6 @@ io.on('connection', function(client){
 			ptys[path].connections++;
 			console.log('Connection open on <' + data + '> (' + ptys[path].connections + ' connected)');
 			var pty = ptys[path];
-			var term = pty.term
 			term.stdout.on('data', function(data) {
 				client.send(data.toString());
 			});
@@ -57,15 +58,15 @@ io.on('connection', function(client){
 	});
   client.on('disconnect', function(){
 		client.pty.connections--;
-		if(client.pty.connections == 0){
+		if(client.pty.connections === 0){
 			client.pty.term.kill();
-			delete ptys[client.pty.path]
+			delete ptys[client.pty.path];
 		}
 		console.log('Connection closed');
   });
 });
 
-server.listen(parseInt(process.argv[2] || '8080', 10));
+server.listen(port);
 
 process.on('uncaughtException', function(err) {
 	console.log(err.stack);
