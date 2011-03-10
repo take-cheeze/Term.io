f368eae6504ba0414396ef1a15a306bccab6cc30
Craig Roddin <craig.roddin@gmail.com>
2011-03-10 11:31:40 -0700

    cleaned up server side code

--- pty.js
+++ pty.js
@@ -4,11 +4,14 @@ var spawn = require('child_process').spawn;
 var fs = require('fs');
 var connect = require('connect');
 var io = require('socket.io');
+var noop = function(){};
 
 var port = parseInt(process.argv[2] || '8080', 10);
 var sslKeyPath = 'privatekey.pem';
 var sslCertPath = 'certificate.pem';
 var useSSL = false;
+var command = 'python';
+var commandArgs = ['-c', 'import pty;pty.spawn(["bash","-l"])'];
 
 var server;
 if(useSSL){
@@ -19,7 +22,8 @@ if(useSSL){
 } else {
 	server = connect();
 }
-
+server.listen(port);
+var io = io.listen(server,{log:noop});
 server.use(function(req, res, next){
     if (/^\/\w+$/.test(req.url)) {
         req.url = '/';
@@ -28,30 +32,26 @@ server.use(function(req, res, next){
 });
 server.use(connect['static'](__dirname + '/static'));
 
-var io = io.listen(server);
 
 var ptys = {};
 io.on('connection', function(client){
 	client.initialized = false;
-	console.log(client);
 	client.on('message', function(data){
 		//The first message sent by the client must be the term they want
 		if(!client.initialized){
 			var path = data;
 			var term;
 			if(!ptys[path]){
-				term = spawn('python', ['-c', 'import pty;pty.spawn(["bash","-l"])']);
-				// or spawn login or just "bash"
-				//It might be better to link to the connections rather than storing a count
+				term = spawn(command,commandArgs);
 				ptys[path] = ptys[path] || {
 					'term': term,
 					'connections':0,
 					'path':path
 				};
 			}
-			term = ptys[path].term
+			term = ptys[path].term;
 			ptys[path].connections++;
-			console.log('Connection open on <' + data + '> (' + ptys[path].connections + ' connected)');
+			console.log('Connection open on ' + data + ' (' + ptys[path].connections + ' connected)');
 			var pty = ptys[path];
 			term.stdout.on('data', function(data) {
 				client.send(data.toString());
@@ -70,16 +70,15 @@ io.on('connection', function(client){
 	});
   client.on('disconnect', function(){
 		client.pty.connections--;
+		console.log('Connection closed on ' + client.pty.path + ' ('+client.pty.connections+' connected)');
 		if(client.pty.connections === 0){
 			client.pty.term.kill();
 			delete ptys[client.pty.path];
 		}
-		console.log('Connection closed');
   });
 });
 
-server.listen(port);
-
+console.log('Ready to accept connections at http'+(useSSL?'s':'')+'://localhost:'+port);
 process.on('uncaughtException', function(err) {
 	console.log(err.stack);
 });
